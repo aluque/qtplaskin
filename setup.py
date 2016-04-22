25a89a4446debed90bac957cc3bd5a2a374a2b5c
Erwan Pannier <erwan.pannier@gmail.com>
2016-04-22 16:35:28 +0200

    install as a package

--- setup.py
+++ setup.py
@@ -1,82 +1,54 @@
-import sys
-from distutils.core import setup
-#from setuptools import setup
-import shutil
-from subprocess import call
+# -*- coding: utf-8 -*-
+"""
+QtPlaskin
 
-import matplotlib
-try:
-    import py2exe
-except ImportError:
-    pass
+A graphical tool to explore ZdPlaskin Results
 
-APP = ['qtplaskin.py']
-PY2APP_OPTIONS = {
-    'iconfile': 'qtplaskin3.icns',
-    'argv_emulation': False,
-    'semi_standalone': False,
-    # 'strip': False,
-    'includes': ['sip', 'PyQt4',
-                 'PyQt4.QtCore', 'PyQt4.QtGui',
-                 #'PyQt4.QtXml', #'PyQt4.QtMultimedia',
-                 'matplotlib', 'numpy', 'scipy', 
-                 'h5py',
-                 'h5py.defs',
-                 'h5py.h5ac',
-                 'h5py._errors',
-                 'h5py._objects', 
-                 'h5py.defs', 
-                 'h5py.utils',
-                 'h5py._proxy'],
-    'frameworks': ['/opt/local/share/qt4/plugins/imageformats/libqgif.dylib',],
-    'excludes': ['libQtGui.4.dylib', 'libQtGui.4.dylib'],
-    
-    #              'PyQt4.QtDesigner', 'PyQt4.QtNetwork',
-    #              'PyQt4.QtOpenGL', 'PyQt4.QtScript', 'PyQt4.QtSql',
-    #              'PyQt4.QtTest', 'PyQt4.QtWebKit', 'PyQt4.QtXml',
-    #              'PyQt4.phonon'],
-    'resources': ['qtplaskin3.png',
-                  '/opt/local/lib/Resources/qt_menu.nib',
-                  'modeldata.py', 'mainwindow.py', 'zdplaskin.py',
-                  'runner.py', 'mplwidget.py'],
-    }
- 
-# 'exclude_package_data': {'src':['*.c', '*.h',  '*.pyx', '*.pxd']}
-PY2EXE_OPTIONS = {
-    "dll_excludes": ["MSVCP90.dll"],
-    "includes": ["sip", "h5py.*"]}
+https://github.com/aluque/qtplaskin
 
-shutil.rmtree('dist', ignore_errors=True)
+Written by Alejandro Luque
 
-if sys.argv[1] == 'py2exe':
-    from distutils.core import setup
-    setup(
-        app=APP,
-        options={'py2app': PY2APP_OPTIONS,
-                 'py2exe': PY2EXE_OPTIONS},
-        windows=['qtplaskin.py'],
-        setup_requires=['py2app'],
-        data_files=matplotlib.get_py2exe_datafiles())
-    shutil.move('dist', 'qtplaskin-win')
-    call("zip qtplaskin-win.zip qtplaskin-win/*", shell=True)
-    
+---
+
+Updated by Erwan Pannier, 2016 to turn it into an app
 
 
-elif sys.argv[1] == 'py2app':
-    from setuptools import setup
-    setup(
-        name="QtPlaskin",
-        version="0.2",
-        app=APP,
-        options={'py2app': PY2APP_OPTIONS,
-                 'py2exe': PY2EXE_OPTIONS},
-        data_files=matplotlib.get_py2exe_datafiles())
+"""
 
-    # py2app does not handle plugins correctly, so we copy them manually
+import os
+from setuptools import setup
+import codecs
 
-    # Be careful with the absolute paths here.  They may change.
-    shutil.copytree("/opt/local/share/qt4/plugins",
-                    "dist/qtplaskin.app/Contents/MacOS/plugins")
-    shutil.copy("qt.conf", "dist/qtplaskin.app/Contents/Resources/")
-    shutil.copy("README", "dist/README.txt")
-    call("hdiutil create QtPlaskin.dmg -ov -srcfolder dist/", shell=True)
+long_description = 'A graphical tool to explore ZdPlaskin Results'
+if os.path.exists('README.md'):
+    long_description = codecs.open('README.md', encoding="utf-8").read()
+        
+setup(name='publib',
+      version='1.0.1',
+      description='A graphical tool to explore ZdPlaskin Results',
+    	long_description=long_description,
+      url='https://github.com/erwanp/publib',
+      author='Alejandro Luque. Updated by Erwan Pannier',
+      author_email='erwan.pannier@gmail.com',
+      packages=['qtplaskin'],
+      install_requires=[
+          'builtins',
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'pyqt4',
+          'h5py',
+          'mpldatacursor'
+		  ],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Text Processing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        "Operating System :: OS Independent"],
+	  include_package_data=True,
+      zip_safe=False)
