5e60e308b80deebcb1b1fb2ccd9fb845c56470d2
Erwan Linux <erwan.pannier@gmail.com>
2018-05-04 13:47:53 +0200

    update test case

--- test/run_test.py
+++ test/run_test.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 """
+How to use?
+
+ZdPlaskin output files are not embedded in QtPlaskin, as they are very heavy
+
+You need to generate a first set of output files. Just run whatever case you 
+have available
+
+Then place it in a Results/ folder in qtplaskin/test
+
+Dont forget never to git these files 
 """
 
 import qtplaskin
 
-qtplaskin.main.load('./test0/')
+qtplaskin.main.load('./Results')
